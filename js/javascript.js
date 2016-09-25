6debbbe6805353106d40f4098a950c726a05b84f
Aubrey Cook <aubreyjcook@gmail.com>
2016-09-25 14:54:05 -0500

    stuck on infinite loop

--- js/javascript.js
+++ js/javascript.js
@@ -2,9 +2,54 @@
 //alert("Page Loaded");
 
 
+var timer;
+var image = new Image();
+var currentSlide = 0;
+
+
+var imageArray = ["images/picture01.jpg","images/picture02.jpg","images/picture03.jpg","images/picture04.jpg","images/picture05.jpg","images/picture06.jpg","images/picture07.jpg","images/picture08.jpg","images/picture09.jpg","images/picture10.jpg","images/picture11.jpg"];
+
+//debug message
+//alert("Variables Loaded");
+
+var imageArrayPreload = function() {
+    for (var i = 0; i < imageArray.length; i++) {
+        image.src = imageArray[i];
+        imageArray.push(image);
+    }
+}
+
+var activateSlideShow = function () {
+    var path = document.getElementById("activateButton").innerHTML;
+    var testState = new RegExp("play");
+    
+    
+    if(testState.test(path)) {
+        document.getElementById("activateButton").innerHTML = "Stop";
+        slideShowActive();
+    }
+    
+    else {
+        document.getElementById("activateButton").innerHTML = "Start";
+        clearTimeout(timer);
+    }
+}
+
+var slideShowActive = function (){
+    if (currentSlide > 11) {
+        currentSlide = 0;
+    }
+    
+    document.getElementById("slideShowImage").src = images[currentSlide];
+    
+    currentSlide++;
+    timer = setTimeout(slideShowActive, 5000);
+}
+
 //function runtime at page load
 window.onload = function () {
     //debug message
     //alert("Functions Loaded");
-
+    
+    imageArrayPreload();
 }
